7535781c8de0994e7cbe005c1ca8468b393db71e
Brian Best <brian.brianb64@gmail.com>
2021-07-05 15:56:59 -0400

    Hook up firebase

---
+++ app.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC98zxXrBwEGsMqSX53BY_MLp3qD7rp6XA",
+  authDomain: "elvaandbrian-com.firebaseapp.com",
+  projectId: "elvaandbrian-com",
+  storageBucket: "elvaandbrian-com.appspot.com",
+  messagingSenderId: "51926797195",
+  appId: "1:51926797195:web:628642d35cbbb5603c3d8e",
+  measurementId: "G-X7QXK8K13W"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+const db = firebase.firestore();
+
+db.collection("movies").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    const movie = doc.data();
+    $('#movie-list').append(`<li>${movie.title}</li>`);
+  });
+});
